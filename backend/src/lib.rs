fec9debe81b5a9ce42e867ffc7095ecb68f15e80
Vimal Patel <vimal@imorphr.com>
2022-02-21 13:04:49 +0000

    Add backend logic to calculate mersenne prime

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,8 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use primes::is_prime;
+
+fn find_next_mersenne_prime(earlier_prime: u32) {
+    let mut p = earlier_prime + 1;
+    while !lucas_lehmer_test(p) {
+        p += 1;
     }
+}
+
+fn lucas_lehmer_test(n: u32) -> bool {
+    if n == 2 {
+        return true;
+    }
+
+    if !is_prime(n.into()) {
+        return false;
+    }
+
+    let m = u32::pow(2, n) - 1;
+    let mut s = 4;
+
+    for _ in 2..n {
+        let square = s * s;
+        s = (square & m) + (square >> n);
+        if s >= m {
+            s -= m;
+        }
+
+        s -= 2;
+    }
+
+    return s == 0;
 }
